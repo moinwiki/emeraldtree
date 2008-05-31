9fa75f7a68fb8991e1fbad187b994b519f011dce
Bastian Blank <bblank@thinkmo.de>
2008-05-31 15:10:47 +0200

    Add testcases.
    * emeraldtree/tests/__init__.py: Add.
    * emeraldtree/tests/test_ElementTree.py: Add testcases.

---
+++ emeraldtree/tests/test_ElementTree.py
@@ -0,0 +1,19 @@
+from emeraldtree.ElementTree import ElementTree, Element
+
+def serialize(elem, **options):
+    from cStringIO import StringIO
+    file = StringIO()
+    tree = ElementTree(elem)
+    tree.write(file, **options)
+    return file.getvalue()
+
+def test_Element___init__():
+    elem = Element('a')
+    assert serialize(elem) == '<a />'
+
+def test_Element_append():
+    elem = Element('a')
+    elem.append(Element('b'))
+    assert serialize(elem) == '<a><b /></a>'
+    elem.append(Element('c'))
+    assert serialize(elem) == '<a><b /><c /></a>'
