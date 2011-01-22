7a1bee41dee180c2e465d189de59a4e2ee30520b
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2011-01-22 02:01:26 +0100

    setup.py: use setuptools, add more metadata for pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,51 @@
 #!/usr/bin/env python
+"""
+EmeraldTree, a light-weight toolkit for XML processing in Python
+================================================================
 
-from distutils.core import setup
+EmeraldTree is a fork of ElementTree - the main differences are:
+
+* It has a slightly different API:
+
+  - Handling of text: it uses unicode objects as children (not as
+    "tail" attributes of the elements)
+
+* API cleanups, removing backward compatibility 
+
+* Better unicode support
+
+* Other improvements / optimizations
+
+The fork happened at ElementTree 1.3a3-20070912-preview.
+
+For more details please see the `EmeraldTree repository <http://hg.moinmo.in/EmeraldTree/>`_.
+
+"""
+
+from setuptools import setup
 
 DESCRIPTION="EmeraldTree - a light-weight XML object model for Python."
 
 setup(
     name="emeraldtree",
-    version="undefined",
+    version="0.9.0",
+    url='http://hg.moinmo.in/EmeraldTree/',
+    download_url = "http://static.moinmo.in/files/packages/emeraldtree-0.9.0.tar.gz",
     author="Bastian Blank",
     author_email="bblank@thinkmo.de",
+    keywords=["xml", "html", "element", "tree", "dom", "unicode", ],
     description=DESCRIPTION,
+    long_description=__doc__,
     license="Python (MIT style)",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Text Processing :: Markup :: XML',
+    ],
     packages=["emeraldtree"],
     platforms="Python 2.4 and later.",
-    )
+)
